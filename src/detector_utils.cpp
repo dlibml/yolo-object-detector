979e07b2db20330018ebf4324f3b4e0e09d575b0
Adrià Arrufat <adria.arrufat@omnious.com>
2021-12-07 05:16:29 +0900

    clean up code

--- src/detector_utils.cpp
+++ src/detector_utils.cpp
@@ -18,23 +18,15 @@ void postprocess_detections(
 
 void setup_detector(net_train_type& net, const dlib::yolo_options& options)
 {
-    // clang-format off
-    visit_computational_layers(net, [](dlib::leaky_relu_& l) { l = dlib::leaky_relu_(0.1); });
-    visit_computational_layers(dlib::layer<rgpnet::btag4>(net), [](auto& l) { dlib::disable_bias(l); });
-    // dlib::visit_computational_layers(net, [](auto& l)
-    // {
-    //     dlib::set_learning_rate_multiplier(l, 1);
-    //     dlib::set_bias_learning_rate_multiplier(l, 1);
-    //     dlib::set_weight_decay_multiplier(l, 1);
-    //     dlib::set_bias_weight_decay_multiplier(l, 1);
-    // });
-    // clang-format on
-    dlib::disable_duplicative_biases(net);
+    using namespace dlib;
+    visit_computational_layers(net, [](leaky_relu_& l) { l = leaky_relu_(0.1); });
+    visit_computational_layers(layer<rgpnet::btag4>(net), [](auto& l) { disable_bias(l); });
+    // disable_duplicative_biases(net);
     const long num_classes = options.labels.size();
-    const long num_anchors_1 = options.anchors.at(dlib::tag_id<ytag8>::id).size();
-    const long num_anchors_2 = options.anchors.at(dlib::tag_id<ytag16>::id).size();
-    const long num_anchors_3 = options.anchors.at(dlib::tag_id<ytag32>::id).size();
-    dlib::layer<ytag8, 2>(net).layer_details().set_num_filters(num_anchors_1 * (num_classes + 5));
-    dlib::layer<ytag16, 2>(net).layer_details().set_num_filters(num_anchors_2 * (num_classes + 5));
-    dlib::layer<ytag32, 2>(net).layer_details().set_num_filters(num_anchors_3 * (num_classes + 5));
+    const long num_anchors_1 = options.anchors.at(tag_id<ytag8>::id).size();
+    const long num_anchors_2 = options.anchors.at(tag_id<ytag16>::id).size();
+    const long num_anchors_3 = options.anchors.at(tag_id<ytag32>::id).size();
+    layer<ytag8, 2>(net).layer_details().set_num_filters(num_anchors_1 * (num_classes + 5));
+    layer<ytag16, 2>(net).layer_details().set_num_filters(num_anchors_2 * (num_classes + 5));
+    layer<ytag32, 2>(net).layer_details().set_num_filters(num_anchors_3 * (num_classes + 5));
 }
