ee3c7559d26f7232ab4796bd92700cfc42f478bb
Adrià Arrufat <adria.arrufat@omnious.com>
2022-04-12 16:57:22 +0900

    Do not use else after return

--- src/detector_utils.cpp
+++ src/detector_utils.cpp
@@ -8,22 +8,16 @@ dlib::rectangle_transform preprocess_image(
     const long stride)
 {
     if (use_letterbox)
-    {
         return dlib::rectangle_transform(inv(letterbox_image(image, output, image_size)));
-    }
-    else
-    {
-        const double width = image.nc();
-        const double height = image.nr();
-        const auto scale = image_size / std::max<double>(height, width);
-        output.set_size(
-            (static_cast<long>(height * scale + 0.5) / stride) * stride,
-            (static_cast<long>(width * scale + 0.5) / stride) * stride);
-        resize_image(image, output);
-        return dlib::point_transform_affine(
-            {width / output.nc(), 0, 0, height / output.nr()},
-            {0, 0});
-    }
+
+    const double width = image.nc();
+    const double height = image.nr();
+    const auto scale = image_size / std::max<double>(height, width);
+    output.set_size(
+        (static_cast<long>(height * scale + 0.5) / stride) * stride,
+        (static_cast<long>(width * scale + 0.5) / stride) * stride);
+    resize_image(image, output);
+    return dlib::point_transform_affine({width / output.nc(), 0, 0, height / output.nr()}, {0, 0});
 }
 
 void postprocess_detections(
