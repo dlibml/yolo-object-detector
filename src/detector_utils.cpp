4919b93c18040b965b5aef594b73c320388c54f1
Adrià Arrufat <adria.arrufat@omnious.com>
2022-05-04 16:33:24 +0900

    Fix preprocess logic without letterbox

--- src/detector_utils.cpp
+++ src/detector_utils.cpp
@@ -14,8 +14,8 @@ dlib::rectangle_transform preprocess_image(
     const double height = image.nr();
     const auto scale = image_size / std::max<double>(height, width);
     output.set_size(
-        (lround(height * scale) / stride) * stride,
-        (lround(width * scale) / stride) * stride);
+        lround(height * scale / stride) * stride,
+        lround(width * scale / stride) * stride);
     resize_image(image, output);
     return dlib::point_transform_affine({width / output.nc(), 0, 0, height / output.nr()}, {0, 0});
 }
